4e6d44156a90473fa799f9bc4e7b7a4884ba22c0
Ayberk Aydemir <onlybriefcase@gmail.com>
2025-06-13 19:30:56 +0300

    add routeListComponent.ts

--- src/app/components/routesList.component.ts
+++ src/app/components/routesList.component.ts
@@ -1,13 +1,38 @@
-import { Component } from "@angular/core";
+import { Component, Inject } from "@angular/core";
+import { RoutesService } from "./routesService.service";
+import { Routee } from "./routee.interface";
+import { AsyncPipe, NgFor } from "@angular/common";
+import { interval, Observable, take, toArray } from "rxjs";
 
 
 @Component({
     selector: 'routes-list',
-    imports: [],
+    imports: [NgFor],
+    providers: [RoutesService],
     templateUrl: './routesList.template.html',
     styleUrl: './routesList.styles.scss'
 })
 export class RoutesList {
     title = 'routes-list';
+
+    routes: Routee[] = [];
+    pageNumbs: number[] = [];
+
+    constructor(public routesApi: RoutesService) {
+        this.setRoutes();
+    }
+
+
+    private setRoutes() {
+        this.routes = this.routesApi.getItems().filter((v, id) => id < 10);
+        var totalPages = this.routesApi.getItems().length / 10;
+        for (let q = 1; q <= totalPages; q++) {
+            this.pageNumbs[q - 1] = q;
+        }
+    }
+
+
+
+
 
 }
