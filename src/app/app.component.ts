5e015ab14883242cf8beea6daab33584129e4254
Ayberk Aydemir <onlybriefcase@gmail.com>
2025-06-13 17:06:06 +0300

    app init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RoutesList } from './components/routesList.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RoutesList],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'routes-list';
+
 }
---
+++ src/app/components/routesList.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+
+
+@Component({
+    selector: 'routes-list',
+    imports: [],
+    templateUrl: './routesList.template.html',
+    styleUrl: './routesList.styles.scss'
+})
+export class RoutesList {
+    title = 'routes-list';
+
+}
