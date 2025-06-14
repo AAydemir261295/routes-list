c69a27b6c60679e44805fa4a0fda07e6b880fc55
Ayberk Aydemir <onlybriefcase@gmail.com>
2025-06-14 12:50:43 +0300

    edit routesList service

--- src/app/components/routesService.service.ts
+++ src/app/components/routesService.service.ts
@@ -1,9 +1,13 @@
-import { Injectable, InjectionToken } from "@angular/core";
+import { Injectable } from "@angular/core";
 import { v4 as uuidv4 } from 'uuid';
 import { Routee } from "./routee.interface";
 
 const interfaceTypes = [
     "Подключение Ethernet", "Гостевая сеть", "Домашная сеть", "Подключение Ethernet"
+]
+
+const ipClass = [
+    24, 25, 32
 ]
 
 
@@ -20,16 +24,25 @@ export class RoutesService {
         for (let q = 0; q < 100; q++) {
             this.items.push({
                 uuid: uuidv4(),
-                address: this.getAddressGatewayMask(),
-                mask: this.getAddressGatewayMask(),
-                gateway: this.getAddressGatewayMask(),
+                address: this.getAddress(),
+                mask: this.getGatewayMask(),
+                gateway: this.getGatewayMask(),
                 interface: interfaceTypes[Math.floor(Math.random() * (3 - 0 + 1) + 0)],
             })
         }
     }
 
+    private getAddress() {
+        var ipAddressGatewayMask = (Math.floor(Math.random() * 255) + 1) +
+            "." + (Math.floor(Math.random() * 255))
+            + "." + (Math.floor(Math.random() * 255))
+            + "." + (Math.floor(Math.random() * 255))
+            + "/" + ipClass[Math.floor(Math.random() * 2) + 1];
+        return ipAddressGatewayMask;
+    }
 
-    private getAddressGatewayMask() {
+
+    private getGatewayMask() {
         var ipAddressGatewayMask = (Math.floor(Math.random() * 255) + 1) +
             "." + (Math.floor(Math.random() * 255))
             + "." + (Math.floor(Math.random() * 255))
